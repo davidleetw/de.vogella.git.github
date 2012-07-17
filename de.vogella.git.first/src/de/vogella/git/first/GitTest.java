f915bc77e75813c6e8d391148f857d4c29c4050d
david lee <david.lee.tk@gmail.com>
2012-07-17 14:38:21 +0800

    2012、07、17

---
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -0,0 +1,13 @@
+package de.vogella.git.first;
+
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git is fun");
+	}
+
+}
